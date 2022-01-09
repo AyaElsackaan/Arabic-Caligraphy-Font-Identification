56a96d7edde6aaf99c702487db7b75814b532bff
Aya-Mahmoud-99 <ayamahmoudabdelfatah99@gmail.com>
2022-01-09 12:49:09 +0200

    lpq

--- pre_processing.py
+++ pre_processing.py
@@ -2,13 +2,24 @@ import numpy as np
 def pre_process(x):
     #bet7awel le binary
     color=(np.max(x)+np.min(x))/2
+    print("hehhh")
     if x[0][0][0]>color:
         return (x<color).astype(int)
     else:
         return (x>color).astype(int)
+
+def pre_process_gray(x):
+        # bet7awel le binary
+        color = (np.max(x) + np.min(x)) / 2
+        print("hehhh")
+        if x[0][0][0] > color:
+            return (x < color).astype(int)
+        else:
+            return (x > color).astype(int)
 def crop_image(img,tol=0):
     # img is 2D image data
     # tol  is tolerance
     mask = img>tol
+    print(mask)
     #print(mask.any(1),mask.any(0))
     return img[np.ix_(mask.any(1).reshape(-1),mask.any(0).reshape(-1))]
